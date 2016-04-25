aeb7eb2aa61397c28c5231f6ba3d2afb09f4fc08
jimmyLi <liviu.c.simion@gmail.com>
2016-04-25 16:53:24 +0200

    updated navigation bar style

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
 console.log('\'Allo \'Allo!');
+
+(function(){
+	var app = angular.module('store', []);
+
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	var gems = [
+		{
+			title = 'Pink Shoes',
+			subtitle = 'Now at $145,99'
+		},
+		{
+			title = 'Anna Field',
+			subtitle = 'Limited Edition'
+		}
+	];
+})();
