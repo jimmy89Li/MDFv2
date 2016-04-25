a3a51dc139719c98d01963db4b7822618cf1d376
jimmyLi <liviu.c.simion@gmail.com>
2016-04-25 19:38:07 +0200

    updated main page + style

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,16 @@
 console.log('\'Allo \'Allo!');
 
-(function(){
-	var app = angular.module('store', []);
-
-	app.controller('StoreController', function(){
-		this.products = gems;
+angular.module('store', [])
+	.controller('StoreController', function(){
+		var product = this;
+		product.tabs = [
+			{
+				name: 'Pink Shoes',
+				subname: 'Now at 145',
+			},
+			{
+				name: 'Anna Field',
+				subname: 'Limited Edition',
+			}
+		];
 	});
-
-	var gems = [
-		{
-			title = 'Pink Shoes',
-			subtitle = 'Now at $145,99'
-		},
-		{
-			title = 'Anna Field',
-			subtitle = 'Limited Edition'
-		}
-	];
-})();
